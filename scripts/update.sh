77671f3de2e397f3f433e6b6e8471d53ffb8c52b
shanpark <shanpark@naver.com>
2021-09-06 22:55:10 +0900

    push for test

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,6 +3,23 @@
 cd ../langs
 date "+%Y%m%d%H%M%S" > version.txt
 
-ls -1 -d */ | cut -f1 -d'/' | cut -f2 -d'.' > list.txt
+ls -1 -d */ | cut -f1 -d'/' > list.txt
 echo >> list.txt
-ls -1 -d */ | cut -f1 -d'/' >> list.txt
+ls -1 -d */ | cut -f1 -d'/' | cut -f2 -d'.' >> list.txt
+
+while read dir; do
+
+if [ -z "$dir" ]
+then
+    break;
+fi
+
+cd $dir
+
+ls -1 *.md | cut -f1 -d'.' > list.txt
+
+cd ..
+
+echo [$dir]
+
+done < list.txt
