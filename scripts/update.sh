1b0dea80c74cf3e298b827034c75ffc256d7d1c3
shanpark <shanpark@naver.com>
2021-09-09 19:44:00 +0900

    .

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
+# 파일 버전 생성.
 cd ../langs
-date "+%Y%m%d%H%M%S" > version.txt
+date +%s%3N > version.txt
 
+# language list.txt 작성. path 먼저쓰고 한줄 띠고 name쓰고...
 ls -1 -d */ | cut -f1 -d'/' > list.txt
 echo >> list.txt
-ls -1 -d */ | cut -f1 -d'/' | cut -f2 -d'.' >> list.txt
+ls -1 -d */ | cut -f1 -d'/' | cut -f2 -d'-' >> list.txt
 
 while read dir; do
 
@@ -14,12 +16,12 @@ then
     break;
 fi
 
+echo "Enter [$dir]"
 cd $dir
 
-ls -1 *.md | cut -f1 -d'.' > list.txt
+ls -1 *.md | cut -f1 -d'.' | cut -f2 -d'-' > list.txt
 
+echo "Leave [$dir]"
 cd ..
-
-echo [$dir]
 
 done < list.txt
