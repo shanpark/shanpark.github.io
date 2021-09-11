06e08887364b9ca070a6366ca350eb407693743a
shanpark <shanpark@naver.com>
2021-09-11 16:09:37 +0900

    list.txt format 변경.

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,22 +4,15 @@
 cd ../langs
 date +%s > version.txt
 
-# language list.txt 작성. path 먼저쓰고 한줄 띠고 name쓰고...
+# language list.txt 작성.
 ls -1 -d */ | cut -f1 -d'/' > list.txt
-echo >> list.txt
-ls -1 -d */ | cut -f1 -d'/' | cut -f2 -d'-' >> list.txt
 
 while read dir; do
-
-if [ -z "$dir" ]
-then
-    break;
-fi
 
 echo "Enter [$dir]"
 cd $dir
 
-ls -1 *.md | cut -f1 -d'.' | cut -f2 -d'-' > list.txt
+ls -1 *.md | cut -f1 -d'.' > list.txt
 
 echo "Leave [$dir]"
 cd ..
